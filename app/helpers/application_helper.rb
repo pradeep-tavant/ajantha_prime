cc77ba85735c5f6d6d7901b6bfd77deca4e63291
Pradeepkumar P <pradeepkumar.p@tavant.com>
2018-12-24 17:18:43 +0530

    Login changed to dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,21 @@ module ApplicationHelper
 
     html.html_safe
   end
+
+  def flats_list
+    registered = Member.all.map(&:login)
+    available = []
+    ['A','B'].each do |block|
+      last_flat = block == 'A' ? 11 : 12
+      ['GF','FF','SF','TF','FOF'].each do |floor|
+        (1..last_flat).to_a.map{|x| ('%02d' % x)}.each do |flat|
+          flat_str = (floor + flat + "-" + block)
+          if registered.include?(flat_str)
+            available << flat_str
+          end
+        end
+      end
+    end
+    available
+  end
 end
--- lib/unregistered.rb
+++ lib/unregistered.rb
@@ -1,8 +1,9 @@
 registered = Member.all.map(&:login)
 available = []
 ['A','B'].each do |block|
+  last_flat = block == 'A' ? 11 : 12
   ['GF','FF','SF','TF','FOF'].each do |floor|
-    (1..12).to_a.map{|x| ('%02d' % x)}.each do |flat|
+    (1..last_flat).to_a.map{|x| ('%02d' % x)}.each do |flat|
       available << (floor + flat + "-" + block)
     end
   end
