9afd85f3874c9ef06718c1a626575bed62304735
Pradeepkumar P <pradeepkumar.p@tavant.com>
2019-01-09 12:16:58 +0530

    Contacts displayed to public

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,4 +1,4 @@
 class Facility < ApplicationRecord
   validates :name, :category, :phone, presence: true
-  enum category: {manager: 1, security: 2, house_keeping: 3, stp: 4, wtp: 5, lift: 6, gym: 7, play_area: 8, pool: 9, electrician: 10, plumber: 11, other: 12}
+  enum category: ["Manager", "Security", "HouseKeeping", "Stp", "Wtp", "Lift", "Gym", "PlayArea", "Pool", "Electrician", "Plumber", "Garbage", "Grills", "Other"]
 end
