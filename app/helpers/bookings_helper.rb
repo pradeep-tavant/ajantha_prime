c537820b462c501227a41fcc17b4cbeb18e8ed9f
Pradeepkumar P <pradeepkumar.p@tavant.com>
2019-01-23 13:58:39 +0530

    Party hall booking rules added

---
+++ app/helpers/bookings_helper.rb
@@ -0,0 +1,18 @@
+module BookingsHelper
+  def party_hall_rules
+    [
+      "Booking of the Party Hall is on a <b>first come first serve basis</b>.",
+      "Once the Booking is <b>Approved</b>, the residents using the Party Hall will pay in advance the charges levied by the Managing Committee (which is <b>Rs.500/-</b>) to the Treasurer/Maintenance Manager/Online (also update Transaction details in website if paid online) and receive appropriate receipt for the same.",
+      "Extreme care has to be taken while putting up decorative items. Please ensure that the wall and other parts of the hall are free of adhesive tapes / decorative items before handing over. Please use dustbins for all waste and clean up buntings and balloons after party. Clean up charges are applicable, if plates, balloons or any kind of items are thrown around in Party Hall during handover.",
+      "Cooking at the Party Hall is not allowed. Food items remaining should be disposed off properly and NOT be disposed off into the bathroom/toilet. It is the responsibility of the resident to inform the caterers or their staff accordingly.",
+      "Guests attending parties are not allowed to use any of the facilities like the swimming pool, gym etc.",
+      "Any damage to the party hall premises will be borne by the resident who has booked the party hall.",
+      "Residents or their guests cannot use party halls for lodging purpose.",
+      "The Party hall should be handed over to the management after the use with due minimal cleaning.",
+      "The Party hall shall be open for parties / promotions / demos / exhibitions etc. from 6 AM to 10 PM only.",
+      "All residents and their guests should maintain decency and decorum within the Party Hall.",
+      "All residents shall be bound by the rules & regulations framed by the Managing Committee in respect of entry and usage of the Party Hall.",
+      "In case of any questions, contact Maintenance Manager or reach out to EC members."
+    ]
+  end
+end
