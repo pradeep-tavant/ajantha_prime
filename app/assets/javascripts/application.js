288b97a731f363a8a770546ad700b5255da14799
Pradeepkumar P <pradeepkumar.p@tavant.com>
2019-02-18 16:49:36 +0530

    Jquery touch reverted

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.touch
 //= require dataTables/jquery.dataTables
 //= require dataTables/extras/dataTables.responsive
 //= require cocoon
