ec82630720e4d1bf69448bafd9045811d0a09847
Pradeepkumar P <pradeepkumar.p@tavant.com>
2019-02-26 13:07:06 +0530

    Polling changes

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -5,7 +5,7 @@ class Poll < ApplicationRecord
   has_many :vote_options, dependent: :destroy
   accepts_nested_attributes_for :vote_options, :reject_if => :all_blank, :allow_destroy => true
 
-  validates :topic, presence: true
+  validates :topic, :start_date, :end_date, :vote_options, presence: true
 
   def normalized_votes_for(option)
     votes_summary == 0 ? 0 : (option.votes.count.to_f / votes_summary) * 100
